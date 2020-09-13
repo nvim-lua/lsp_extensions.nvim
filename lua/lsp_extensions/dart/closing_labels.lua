836311b5520bf8c3330ae6cc4ddfccfb6bc81f11
Pat O'Connor <patoconnor43@gmail.com>
2020-09-12 18:52:28 -0700

    Add support for dart closing labels

---
+++ lua/lsp_extensions/dart/closing_labels.lua
@@ -0,0 +1,76 @@
+--[[
+## Closing Labels
+
+** Method: 'dart/textDocument/publishClosingLabels**
+Direction: Server -> Client Params: { uri: string, labels: { label: string, range: Range }[] }
+This notifies the client when closing label information is available (or updated) for a file.
+
+Since this is a notification, the callback needs to be registered in the client's callbacks table.
+This can be achieved with nvim_lspconfig with this minimal config.
+```lua
+nvim_lsp.dartls.setup{
+  init_options = {
+    closingLabels = true,
+  },
+  callbacks = {
+    ['dart/textDocument/publishClosingLabels'] = require('lsp_extensions.dart.closing_labels').get_callback{},
+  },
+}
+```
+
+https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#darttextdocumentpublishclosinglabels-notification
+--]]
+
+local M = {}
+-- Stored labels from the server's notifications keyed by uri
+local all_labels = {}
+-- Namespace for the virtual text
+local closing_labels_ns = vim.api.nvim_create_namespace('lsp_extensions.dart.closing_labels')
+-- Stored opts from the get_callback invocation.
+local label_opts = {}
+
+-- Gets a callback to register to the dartls publishClosingLabels notification.
+-- @tparam table a table of options: highlight, prefix
+M.get_callback = function(opts)
+  label_opts.highlight = opts.highlight or "Comment"
+  label_opts.prefix = opts.prefix or " // "
+
+  vim.cmd [[augroup DartShowClosingLabels]]
+  vim.cmd [[  au!]]
+  vim.cmd [[  autocmd CursorHold,CursorHoldI *.dart :lua require('lsp_extensions.dart.closing_labels').draw_labels()]]
+  vim.cmd [[augroup END]]
+
+  return function(_, _, result, _, _)
+    local uri = result.uri
+    local labels = result.labels
+    all_labels[uri] = labels
+    if uri == vim.uri_from_bufnr(0) then
+      M.draw_labels()
+    end
+  end
+end
+
+-- Draws closing labels in the current buffer
+M.draw_labels = function()
+  local opts = label_opts
+
+  local highlight = opts.highlight
+  local prefix = opts.prefix
+  local bufnr = 0
+  local uri = vim.uri_from_bufnr(bufnr)
+  local labels = all_labels[uri] or {}
+
+  local display_virt_text = function(label)
+    local end_line = label.range["end"].line
+    local text = prefix .. label.label
+    vim.api.nvim_buf_set_virtual_text(bufnr, closing_labels_ns, end_line, { { text, highlight } }, {})
+  end
+
+
+  vim.api.nvim_buf_clear_namespace(bufnr, closing_labels_ns, 0, -1)
+  for _, label in pairs(labels) do
+    display_virt_text(label)
+  end
+end
+
+return M
